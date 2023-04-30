bec2b700e51dc20955ba603a0e7c1ad23e2c75c4
Aleksandr Tsygankov <tsygankov.aleksandr@gmail.com>
2023-04-30 07:28:22 +0500

    Initial Commit

---
+++ Ciga Watch App/CigaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CigaApp.swift
+//  Ciga Watch App
+//
+//  Created by Aleksandr Tsygankov on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct Ciga_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
